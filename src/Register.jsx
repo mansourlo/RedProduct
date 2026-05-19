49c135e70d285dccc1c4fc25f28efb7c9317d7ce
mansourlo <mvnsour05@gmail.com>
2026-05-19 17:41:24 +0000

    Livrable 1: Page d'inscription

---
+++ src/Register.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function Register(){
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [accept, setAccept] = useState(false);
+
+    function handleInscription(){
+        if(!name || !email || !password){
+            alert("Veuillez remplir tous les champs");
+        }
+
+        if(!accept){
+            alert("Vous devez accepter les conditions pour s'incrire")
+        }
+    }
+
+
+    return(
+        <div className=" flex flex-col justify-center items-center min-h-screen bg-[#2d2f33]">
+            <div className="flex text-center gap-1 mb-3">
+                <svg width="20" height="20" viewBox="0 0 32 36" xmlns="http://www.w3.org/2000/svg">
+                    <polygon 
+                        points="0,0 32,0 32,36 16,26 0,36" fill="white"
+                    />
+                </svg>
+                <span className="text-white font-bold track widest text-lg">RED PRODUCT</span>
+            </div>
+            <div className="w-[300px] h-fit mx-auto my-[10px] px-5 py-[15px] bg-white border border-white rounded">
+                <p className="mb-[15px] text-[1rem] text-left">Inscrivez-vous en tant que Admin</p>
+                <div  className="flex flex-col gap-1">
+                    <label htmlFor="userName" className="text-[0.9rem] mb-[2px] text-gray-500">Nom</label>
+                    <input type="text" name="userName" id="userName" value={name} onChange={(e) => setName(e.target.value)} className="border-b-2 border-gray-300 py-1 outline-none focus:border-black transition-all duration-300"/><br/>
+
+                    <label htmlFor="email" className="text-[0.9rem] mb-[2px] text-gray-500">E-mail</label>
+                    <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border-b-2 border-gray-300 py-1 outline-none focus:border-black transition-all duration-300"/><br/>
+
+                    <label htmlFor="password" className="text-[0.9rem] mb-[2px] text-gray-500">Mot de passe</label>
+                    <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border-b-2 border-gray-300 py-1 outline-none focus:border-black transition-all duration-300"/><br/>
+
+                    <label>
+                        <input type="checkbox" onChange={(e) => setAccept(e.target.value)} checked={accept} className="mr-1 align-middle cursor-pointer"/>
+                        Accepter les termes et la politique
+                    </label><br/>
+
+                    <button type="submit" onClick={handleInscription} className="w-full p-3 text-base text-white bg-[#2d2f33] border-none rounded cursor-pointer">S'inscrire</button>
+                </div>
+            </div>
+            <p className="text-white text-center">
+                Vous avez deja un compte ? <a href="#" className="no-underline text-yellow-400">Se connecter</a>
+            </p>
+        </div>
+    );
+
+}
+
+export default Register;
