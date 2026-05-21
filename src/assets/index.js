985b5cf1cf03e55f6f196f4b0e5dea0da31d1b70
mansourlo <mvnsour05@gmail.com>
2026-05-21 17:14:23 +0000

    Image des hotels

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import azalai from "./azalai.png";
+import pullman2 from "./pullman2.png";
+import lacrose from "./lacrose.png";
+import kfpalace from "./kfpalace.png";
+import radisson from "./radisson.png";
+import palmbeach from "./palmbeach.png";
+import saly from "./saly.png";
+
+export {
+  azalai,
+  pullman2,
+  lacrose,
+  kfpalace,
+  radisson,
+  palmbeach,
+  saly
+};
