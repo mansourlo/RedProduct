13a49a7117b2b6cc315d425d9ccca339ef527150
mansourlo <mvnsour05@gmail.com>
2026-05-22 15:36:00 +0000

    Fichier main

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import Register from './Register.jsx';
 import Dashboard from './Dashboard.jsx';
 import Header from './Header.jsx';
 import Hotels from './Hotels.jsx';
+import NewHotel from './NewHotel.jsx';
 
 function App(){
   return(
     <div>
-      <Hotels/>
+      
     </div>
   )
 }
