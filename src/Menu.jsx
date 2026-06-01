21c8b2404db656b94ba4bfd6312a959442b78213
mansourlo <mvnsour05@gmail.com>
2026-06-01 11:16:14 +0000

    Augmenter le padding dans le navbar

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,7 +4,7 @@ import photo from './assets/self.png';
 
 function Menu(){
     return(
-        <div>
+        <div className="px-1">
             <div className="flex items-center gap-2 mb-6">
                 <svg className="logo-icone" width="20" height="24" viewBox="0 0 20 24" fill="white">
                 <polygon points="0,0 20,0 20,24 10,18 0,24" />
@@ -13,11 +13,11 @@ function Menu(){
             </div>
             <p className="text-white text-sm mb-6">Principal</p>
             <nav className="text-white text-md font-bold">
-                <span className="flex gap-2 mb-6 hover:bg-white hover:text-[#2d2f33] cursor-pointer">
-                        <LayoutDashboard/> Dashboard
+                <span className="flex gap-2 mb-4 py-2 hover:bg-white hover:text-[#2d2f33] cursor-pointer">
+                    <LayoutDashboard/> Dashboard
                 </span>
-                <span className="flex gap-2 hover:bg-white hover:text-[#2d2f33] cursor-pointer">
-                        <Hotel/> Liste des hotels
+                <span className="flex gap-2 py-2 hover:bg-white hover:text-[#2d2f33] cursor-pointer">
+                    <Hotel/> Liste des hotels
                 </span>
             </nav>
             <footer className="flex items-center gap-3 mt-90 border-t-1 border-gray-500 py-3">
